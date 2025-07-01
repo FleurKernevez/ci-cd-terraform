1001fc42ce5b840a93c8a4bb3e3c6aac3646484a
fleurkernevez <fleur@transparence.pro>
2025-07-01 14:10:22 +0200

    avancées terraform - tout fonctionne sauf bdd sql

--- main.tf
+++ main.tf
@@ -9,9 +9,27 @@ terraform {
 
 provider "docker" {}
 
+# Variables
+variable "mongodb_url" {
+  description = "MongoDB connection string"
+  type        = string
+}
+
+variable "port" {
+  description = "Port used by Node server"
+  type        = string
+}
+
+variable "mysql_pwd" {
+  description = "Port used by Node server"
+  type        = string
+}
+
 resource "docker_network" "internal_network"{
     name = "internal-app-network"
 }
+
+// mango
 
 resource "docker_image" "mongo_image" {
     name = "mongo"
@@ -32,12 +50,10 @@ resource "docker_container" "mongo_db" {
     }
 }
 
-resource "docker_image" "node_image" {
-    name = "node"
+// node
 
-    build {
-        context = "${path.module}"
-    }
+resource "docker_image" "node_image" {
+    name = "docker.io/fleurk/node"
 }
 
 resource "docker_container" "node_server" {
@@ -54,15 +70,6 @@ resource "docker_container" "node_server" {
         internal = 5001
     }
 
-    # Volumes
-    volumes {
-        host_path      = abspath(path.module)
-        container_path = "/server"
-    }
-
-    volumes {
-        container_path = "/server/node_modules"
-    }
 
     # Command to run
     command = ["node", "server.js"]
@@ -71,14 +78,83 @@ resource "docker_container" "node_server" {
     depends_on = [docker_container.mongo_db]
 }
 
-# Variables
-variable "mongodb_url" {
-  description = "MongoDB connection string"
-  type        = string
+// my sql
+
+resource "docker_image" "mysql_image" {
+    name = "docker.io/fleurk/mysql"
+
+}
+resource "docker_container" "mysql_image" {
+    name = "mysql_image"
+    image = docker_image.mysql_image.image_id
+    restart = "always"
+
+    env=[
+        "MYSQL_ROOT_PASSWORD=var.mysql_pwd"
+    ]
+
+    networks_advanced {
+        name = docker_network.internal_network.name
+    }
+
+    ports {
+        external = 3306
+        internal = 3306
+    }
 }
 
-variable "port" {
-  description = "Port used by Node server"
-  type        = string
+// python
+
+resource "docker_image" "python_image" {
+    name = "docker.io/fleurk/python:3.9"
+}
+
+resource "docker_container" "python_server" {
+    name = "python_server"
+    image = docker_image.python_image.image_id
+    restart = "always"
+
+    networks_advanced {
+        name = docker_network.internal_network.name
+    }
+
+    ports {
+        external = 8000
+        internal = 8000
+    }
+
+    # Command to run
+    command = ["python", "-m", "uvicorn", "server:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "8000"]
+
+    # Depends on MongoDB
+    depends_on = [docker_container.mysql_image]
+
+}
+
+// react
+
+resource "docker_image" "react_image" {
+    name = "docker.io/fleurk/react"
+}
+
+resource "docker_container" "react" {
+    name = "react"
+    image = docker_image.react_image.image_id
+    restart = "always"
+
+    networks_advanced {
+        name = docker_network.internal_network.name
+    }
+
+    ports {
+        external = 3000
+        internal = 3000
+    }
+
+    # Command to run
+    command = ["npm", "start"]
+
+    # Depends on MongoDB
+    depends_on = [docker_container.mysql_image]
 }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 mongodb_url = "mongodb+srv://admin:root@cluster0.bk9d178.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 port        = "5001"
+mysql_pwd="toto"
